1803bfce2995d84cf12cba407fe5644c914b6cae
uddhav0001 <uddhavjadhav0807@gmail.com>
2024-03-31 17:11:52 +0530

    add code tp master

--- src/Practice/Test1.java
+++ src/Practice/Test1.java
@@ -5,8 +5,11 @@ public class Test1 {
 	public static void main(String[]arg) {
 		
 		
-System.out.println("Helloword");		
-		
+System.out.println("Helloword");
+
+System.out.println("uddahv jadhjav");
+System.out.println("Ahmednahar");
+System.out.println("bajaj ");
 	
 	}
 }
